40f6d244ed2565b9bca2edb0dc8075c4135c22a7
Evan-sp <epspensley@gmail.com>
2025-02-27 06:58:27 -0500

    Scaffolding

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      scale: {
+        '200': '2',
+      },
+    },
+  },
+  plugins: [],
+  mode: 'jit'
+} 
